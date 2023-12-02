31564c59e9056d24fb4598bff7619612093e991d
Doug Nelson <doug@dochne.com>
2023-12-02 18:51:20 +0000

    fix destructuring

--- day02/part2.rb
+++ day02/part2.rb
@@ -6,13 +6,9 @@ value = STDIN.read.lines(chomp: true)
     .map{[_1[1].to_i, _1[2].split("; ").map{|round| round.split(", ")}]}
     .map do |game, rounds|
         rounds.each_with_object Hash.new do |value, obj|
-            value.each do |color|
-                split = color.split(" ")
-                if obj.has_key?(split[1])
-                    obj[split[1]] = [split[0].to_i, (obj[split[1]]).to_i].max
-                else
-                    obj[split[1]] = split[0].to_i
-                end
+            value.each do
+                total, color = _1.split(" ")
+                obj[color] = obj.has_key?(color) ? [total.to_i, obj[color]].max : total.to_i
             end
         end
     end
