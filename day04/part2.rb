14dbead46bc9b749f07c354bfd40bf25710feb00
Doug Nelson <doug@dochne.com>
2023-12-04 10:34:31 +0000

    improve part2

--- day04/part2.rb
+++ day04/part2.rb
@@ -3,11 +3,8 @@ value = STDIN.read.lines(chomp: true)
     .map{_1.match(/^([^:]*):([^|]*)\|(.*)$/)}
     .map{[_1[2].split(" "), _1[3].split(" ")]}
     .map{ _1.intersection(_2).size }
-    .map{ _1 + 1}
     .reverse
-    .reduce [] do {|acc, total|
-        acc << 1 + (total > 1 ? acc[-(total - 1)..].sum : 0)
-    end
+    .reduce([]) {|acc, total| acc << acc.last(total).sum + 1 }
     .sum
 
 p(value)
