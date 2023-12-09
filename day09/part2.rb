a810b1c2ba79b2ad135eb7f34202fe5fc3300218
Doug Nelson <doug@dochne.com>
2023-12-09 09:40:23 +0000

    neaten

--- day09/part2.rb
+++ day09/part2.rb
@@ -1,27 +1,15 @@
 #!/usr/bin/env ruby
 
-class Array
-    def rolling(n)
-        res = []
-        (self.size - (n - 1)).times do |i|
-            res << self[i...i + n]
-        end
-        res
-    end
-end
-
-def calc_diff(seq)
-    stack = [seq]
-    until stack.last.all?{_1 == 0} do
-        stack << stack.last.rolling(2).map{_2 - _1}
-    end
-    stack.reverse.map(&:first).reduce(0) {|acc, line| line - acc }
-end
-
 input = STDIN.read.lines(chomp: true)
     .map(&:split)
     .map{_1.map(&:to_i)}
-    .map{calc_diff(_1)}
+    .map do |seq|
+        stack = [seq]
+        until stack.last.all?{_1 == 0} do
+            stack << stack.last.each_cons(2).map{_2 - _1}
+        end
+        stack.reverse.map(&:first).reduce(0) {|acc, line| line - acc }
+    end
     .sum
 
 p(input)
--- template/part2.rb
+++ template/part2.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
 
-value = STDIN.read.lines(chomp: true)
+input = STDIN.read.lines(chomp: true)
 
-p(value)
+p(input))
