0af8bb7f5dcb97706ea98eb0741a72a35306359a
Doug Nelson <doug@dochne.com>
2023-12-17 17:17:51 +0000

    move to use a node struct

--- day17/part2-attempt2.rb
+++ day17/part2-attempt2.rb
@@ -4,35 +4,33 @@ require "matrix"
 require "colorize"
 require "rb_heap"
 
+Node = Struct.new(:position, :direction, :score, :route)
+
 def walk(grid, start, goal)
-    heap = Heap.new { |a, b| a[2] < b[2] }
-    heap << [start, Vector[0, 0], 0, []]
-    seen = Set.new
+    heap = Heap.new { |a, b| a.score < b.score }
+    heap << Node.new(start, Vector[0, 0], 0, [])
     scores = {}
 
-    while (pos, last_dir, score, route = heap.pop) do
-        return score, route if pos == goal
+    while (node = heap.pop) do
+        return node.score, node.route if node.position == goal
 
         [Vector[-1, 0], Vector[1, 0], Vector[0, -1], Vector[0, 1]].each do |dir|
-            next if dir == last_dir || dir == last_dir * -1
+            next if dir == node.direction || dir == node.direction * -1
 
-            new_score = score
+            new_score = node.score
             1.upto(10) do |distance|
-                next_pos = pos + (dir * distance)
+                next_pos = node.position + (dir * distance)
 
                 break if next_pos[0] < 0 || next_pos[1] < 0 || grid.size <= next_pos[1] || grid[0].size <= next_pos[0]
 
                 new_score += grid[next_pos[1]][next_pos[0]]
-
                 next if distance <= 3
 
-                entry = [next_pos, dir, new_score, route + [next_pos]]
-                next if seen.include? entry
+                next_node = Node.new(next_pos, dir, new_score, node.route + [next_pos])
                 next if scores.fetch([next_pos, dir], Float::INFINITY) <= new_score
 
                 scores[[next_pos, dir]] = new_score
-                seen.add(entry)
-                heap << entry
+                heap << next_node
             end
         end
     end
@@ -47,14 +45,9 @@ score, route = walk(input, Vector[0, 0], Vector[input[0].size - 1, input.size - 
 
 input.each_with_index do |row, row_idx|
     row.each_with_index do |col, col_idx|
-        if route.include?(Vector[col_idx, row_idx])
-            print(col.to_s.red)
-        else
-            print(col.to_s)
-        end
+        print(route.include?(Vector[col_idx, row_idx]) ? col.to_s.red : col.to_s)
     end
     print("\n")
 end
 
 p(score)
-# p(value)
